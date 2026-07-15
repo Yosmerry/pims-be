8aa91aea23b06be8e5edeef1fe98d696c439bc9d
Yos Merry Raditya Putra <yos.m.r.putra@angkasa.net.id>
2026-07-15 10:54:18 +0700

    initial commit

---
+++ src/test/java/com/yosmerry/pims_be/PimsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yosmerry.pims_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
